703d06d0cf8c9edcab8c55474977d4d50f10aa57
Douglas Gomes <douglasvieira17@gmail.com>
2021-03-16 10:02:51 -0300

    Created base script

---
+++ script.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+var axios = require("axios");
+var moment = require("moment");
+
+const DROPLET_ID = process.env.DROPLET_ID;
+const API_TOKEN = process.env.API_TOKEN;
+const INTERVAL = process.env.INTERVAL;
+const MAX_SNAPSHOTS = process.env.MAX_SNAPSHOTS;
+
+(async () => {
+  const uri = "https://api.digitalocean.com/v2";
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  };
+
+  const getSnapshots = async () => {
+    const response = await axios.get(
+      `${uri}/droplets/${DROPLET_ID}/snapshots`,
+      config
+    );
+
+    return response.data.snapshots;
+  };
+
+  const createSnapshot = async () => {
+    const name = moment().utc().format("YYYY-MM-DD HH:mm");
+    const response = await axios.post(
+      `${uri}/droplets/${DROPLET_ID}/actions`,
+      { type: "snapshot", name: name },
+      config
+    );
+
+    console.log(`Created snapshot ${name}.`);
+
+    return response.data;
+  };
+
+  const deleteSnapshot = async snapshotId => {
+    const response = await axios.delete(`${uri}/images/${snapshotId}`, config);
+
+    console.log(`Deleted snapshot ${snapshotId}.`);
+
+    return response.data;
+  };
+
+  const snapshots = await getSnapshots();
+
+  if (snapshots.length) {
+    const now = moment().utc();
+    const firstSnapshot = snapshots[0];
+    const lastSnapshot = snapshots[snapshots.length - 1];
+
+    const diff = Math.abs(moment(lastSnapshot.created_at).diff(now, "minutes"));
+
+    if (diff >= INTERVAL) {
+      createSnapshot();
+    }
+
+    if (snapshots.length > MAX_SNAPSHOTS) {
+      deleteSnapshot(firstSnapshot.id);
+    }
+  }
+})();
